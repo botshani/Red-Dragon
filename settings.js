63fdbc04e21116f0e0ce462de9baa18dbdaf3529
botshani <111548401+botshani@users.noreply.github.com>
2022-08-19 09:13:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -45,7 +45,7 @@ global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "true" // bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 
 global.owner = ['+94761285478']
-global.ownertag = '+380944814219' //your tag number
+global.ownertag = '+94761285478' //your tag number
 global.botname = 'YT╺ SHANI- 𝘉𝘖𝘛' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
 global.author = "YT╺ SHANI " // sticker author
